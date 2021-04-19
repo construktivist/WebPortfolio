afe25d1af40b94c84e6e3dec2cfcb8d5412e822d
Keegan Kelly <keegankelly1@gmail.com>
2021-04-19 16:40:34 -0500

    Finished project section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,70 +9,24 @@
 
     var db = firebase.firestore();
 
-    // projectArr = [
-    //     {
-    //         name: 'blog',
-    //         description:"Kasasa wanted to move their blogs from Wordpress to Hubspot for this project. I was given a Hubspot template to start with and added the header, footer, topics menu, search bar and subscribe pages. I also made sure the styles throughout the template were updated to match Kasasa's brand.",
-    //         repo: null,
-    //         website:"https://www.kasasa.com/blog",
-    //     },
-    //     {
-    //         name: 'quiz',
-    //         description:"This is a quiz template I built for Kasasa to educate their audience in an interactive way. It was built in Hubspot using Bootstrap carousels and Jquery. The template itself is customizable so content managers can build more quizzes easily.",
-    //         repo: null,
-    //         website:"https://www.kasasa.com/lp/borrowers-quiz",
-    //     },
-    //     {
-    //         name: 'dungeon',
-    //         description:"This is a CLI dungeon crawl game that I developed with Node.js. It uses Inquirer.js to prompt questions then pass the answers to handlers. The current version demonstrates the character creator, exploration, combat and loot systems.",
-    //         repo:"https://github.com/construktivist/react-digital-pet",
-    //         website:null
-    //     },
-    //     {
-    //         name: 'monster',
-    //         description:"This is a game that allows a user to create and save a monster that can be used to fight other monsters. This project focuses primarily on using PHP for most of the functionality. The monsters are saved to a database with MySQL.",
-    //         repo:"https://github.com/construktivist/monster-game",
-    //         website:"https://monster-game.herokuapp.com/",
-    //     },
-    // ]
-
-    // function postFire(n, d, r, w) {
-    //     db.collection("projects").add({
-    //         name: n,
-    //         description: d,
-    //         repo: r,
-    //         website: w
-    //     }).then((docRef) => {
-    //         console.log("Document written with ID: ", docRef.id);
-    //     })
-    //     .catch((error) => {
-    //         console.error("Error adding document: ", error);
-    //     });
-    // }
-
-    // projectArr.forEach(e => {
-    //     postFire(e.name, e.description, e.repo, e.website);
-        
-    // });
-
     $(document).ready(function(){
-    db.collection("projects").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(doc.data(doc.id));
-            $('#projectCards').append('<div class="project-card col-sm-12 col-md-6">' +
-            '<div class="project-card-title">' +
-                '<h3>'+ doc.data(doc.id).name +'</h3>' +
-            '</div>' +
-            '<div class="project-card-copy">' +
-                '<p>' + doc.data(doc.id).description +'</p>' +
-            '</div>' +
-            '<div class="project-card-links">' +
-                '<a href="'+ doc.data(doc.id).repo +'" class="btn">repo</a>' +
-                '<a href="' + doc.data(doc.id).website +'" class="btn">website</a>' +
-            '</div>' +
-            '</div>');
+        db.collection("projects").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data(doc.id));
+                $('#projectCards').append(`<div class="project-card col-sm-12 col-md-6 d-flex flex-column">
+                <div class="project-card-title">
+                    <h3>${doc.data(doc.id).name}</h3>
+                </div>
+                <div class="project-card-copy">
+                    <p>${doc.data(doc.id).copy}</p>
+                </div>
+                <div class="project-card-links d-flex align-items-end flex-grow-1">
+                ${doc.data(doc.id).repo == null ? `<div></div>` : `<a href="${doc.data(doc.id).repo}" target="_blank" class="btn">repo</a>`}
+
+                ${doc.data(doc.id).web == null ? `<div></div>` : `<a href="${doc.data(doc.id).web}" target="_blank"class="btn">website</a>`}
+                </div>`);
+            });
         });
-    });
     });
 
     console.log('test')
